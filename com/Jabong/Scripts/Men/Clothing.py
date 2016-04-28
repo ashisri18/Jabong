8751699e9b7bd1d5453a243cf8bec8ad7054c96d
Ashish Srivastava <ashi.sri.18@gmail.com>
2016-04-28 11:29:12 +0530

    Commiting Jabong after adding one more class in scripts.

--- com/Jabong/Scripts/Men/Clothing.py
+++ com/Jabong/Scripts/Men/Clothing.py
@@ -1,6 +1,13 @@
 from com.Jabong.PageRepo.Men.Clothing import *
+#from com.Jabong.PageRepo.Sports
+
 class BuyBlazzer(ClothingCategory):
     cc = ClothingCategory()
+    sc = SportsCategory()
+
     def test_buyBlazzer(cls):
         cls.cc.clickMenBlock()
-        cls.cc.clickSuitsBlazzer_CC()
+        cls.cc.clickSuitsBlazers_CC()
+
+    def test_buyJerseys(cls):
+        cls.sc.clickJerseys_SC()
