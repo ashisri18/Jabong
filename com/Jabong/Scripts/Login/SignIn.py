aa9675ef0eb264ed43cc141998185981c1d88fe9
Ashish Srivastava <ashi.sri.18@gmail.com>
2016-04-29 18:42:35 +0530

    In this commit we have implemented Read data from Excel data based on TestCaseId.

--- com/Jabong/Scripts/Login/SignIn.py
+++ com/Jabong/Scripts/Login/SignIn.py
@@ -3,9 +3,15 @@ from com.Jabong.PageRepo.Home.HomePage import *
 class SignInJabong(SignIn):
 
     signIn = SignIn()
+    testCaseId = 'SignIn_03'
+    sheetName = 'SignInData'
+
+    # def __init__(cls):
+    #     cls.testCaseId = 'SignIn_01'
+    #     cls.sheetName = 'SignInData'
 
     def test_SignInJabong(cls):
         cls.signIn.clickSignInLink()
-        cls.signIn.enterMailId()
-        cls.signIn.enterPassword()
+        cls.signIn.enterMailId(cls.testCaseId, cls.sheetName)
+        cls.signIn.enterPassword(cls.testCaseId, cls.sheetName)
         cls.signIn.clickSignInBtn()
